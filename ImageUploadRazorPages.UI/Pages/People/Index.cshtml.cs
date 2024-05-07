568ad64710bd1b9e85f3e0ad1259d0ff164bc857
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-05-07 11:50:23 +0530

    feat: delete

--- ImageUploadRazorPages.UI/Pages/People/Index.cshtml.cs
+++ ImageUploadRazorPages.UI/Pages/People/Index.cshtml.cs
@@ -1,5 +1,7 @@
 using ImageUploadRazorPages.Data.Models;
 using ImageUploadRazorPages.Data.Repositories;
+using ImageUploadRazorPages.Data.Shared;
+using ImageUploadRazorPages.UI.Shared;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,10 +10,12 @@ namespace ImageUploadRazorPages.UI.Pages.People
     public class IndexModel : PageModel
     {
         private readonly IPersonRepository _personRepository;
+        private readonly IFileService _fileService;
 
-        public IndexModel(IPersonRepository personRepository)
+        public IndexModel(IPersonRepository personRepository,IFileService fileService)
         {
             _personRepository = personRepository;
+            _fileService = fileService;
         }
 
         [BindProperty]
@@ -21,5 +25,38 @@ namespace ImageUploadRazorPages.UI.Pages.People
         {
             PersonList = await _personRepository.GetPeople();
         }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            try
+            {
+                var person = await _personRepository.GetPersonById(id);
+                if (person == null)
+                {
+                    return NotFound();
+                }
+                await _personRepository.DeletePerson(id);
+                if (!string.IsNullOrWhiteSpace(person.ProfilePicture))
+                {
+                    _fileService.DeleteFile(person.ProfilePicture, "Images");
+                }
+                TempData[NotificationTypes.SuccessMessage] = "Deleted successfully";
+            }
+            catch (FileNotFoundException ex)
+            {
+                TempData[NotificationTypes.ErrorMessage] = ex.Message;
+            }
+            catch (InvalidOperationException ex)
+            {
+                TempData[NotificationTypes.ErrorMessage] = ex.Message;
+            }
+
+            catch (Exception ex)
+            {
+                TempData[NotificationTypes.ErrorMessage] = "Something went wrong!!";
+            }
+            return RedirectToPage("/People/Index");
+        }
+
     }
 }
