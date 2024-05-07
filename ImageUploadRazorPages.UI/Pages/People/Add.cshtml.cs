08bf302f727fdf444518e2487ae42fa2a2dc8d94
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-05-07 10:55:46 +0530

    feat: add

--- ImageUploadRazorPages.UI/Pages/People/Add.cshtml.cs
+++ ImageUploadRazorPages.UI/Pages/People/Add.cshtml.cs
@@ -1,5 +1,7 @@
+using ImageUploadRazorPages.Data.Models;
 using ImageUploadRazorPages.Data.Repositories;
 using ImageUploadRazorPages.Data.Shared;
+using ImageUploadRazorPages.UI.Shared;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +11,9 @@ public class AddModel : PageModel
 {
     private readonly IPersonRepository personRepository;
     private readonly IFileService fileService;
+
+    [BindProperty]
+    public Person? Person { get; set; }
 
     public AddModel(IPersonRepository personRepository,IFileService fileService)
     {
@@ -21,6 +26,42 @@ public class AddModel : PageModel
 
     public async Task<IActionResult> OnPostAsync()
     {
-        
+
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+        try
+        {
+            if (Person?.ImageFile!=null)
+            {
+                if (Person.ImageFile.Length > 1 * 1024 * 1024)
+                {
+                    throw new InvalidOperationException("File can not exceed 1 MB");
+                }
+                Person.ProfilePicture = await fileService.SaveFile(Person.ImageFile, "Images", new string[] { ".jpg", ".jpeg", ".png" });
+            }
+            await personRepository.AddPerson(Person);
+            TempData[NotificationTypes.SuccessMessage] = "Saved successfully.";
+            return RedirectToPage("/People/Add");
+        }
+        catch (InvalidOperationException ex)
+        {
+            TempData[NotificationTypes.ErrorMessage] = ex.Message;
+            return Page();
+        }
+        catch (FileNotFoundException ex)
+        {
+            TempData[NotificationTypes.ErrorMessage] = ex.Message;
+            return Page();
+        }
+        catch (Exception ex)
+        {
+            TempData[NotificationTypes.ErrorMessage] = "Something went wrong!";
+            return Page();
+        }
+
+
     }
+
 }
